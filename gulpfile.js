8a551e52a06e2d58a7878c0a32d30a0e392b804c
ZecteD <muh.luthfi@live.com>
2018-11-29 15:48:17 +0700

    test git bash

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,9 @@ var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 var purgecss = require('gulp-purgecss');
 var cleanCss = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
 
 
-
-gulp.task('watch', function(){
-    gulp.watch('src/main.css', ['tailwind']);
-})
 
 gulp.task('tailwind', function () {
     return gulp.src('src/main.css')
@@ -43,22 +40,37 @@ gulp.task('tailwind', function () {
 
 gulp.task('css', function() {
     return gulp.src('build/*.css')
-    // .pipe(purgecss({
-    //     content: ["*.html"],
-    //     extractors: [
-    //         {
-    //             extractor: class TailwindExtractor {
-    //                 static extract(content) {
-    //                     return content.match(/[A-z0-9-:\/]+/g) || [];
-    //                 }
-    //             },
-    //             extensions: ['css', 'html']
-    //         }
-    //     ]
-    // }))
+    .pipe(purgecss({
+        content: ["*.html"],
+        extractors: [
+            {
+                extractor: class TailwindExtractor {
+                    static extract(content) {
+                        return content.match(/[A-z0-9-:\/]+/g) || [];
+                    }
+                },
+                extensions: ['css', 'html']
+            }
+        ]
+    }))
     .pipe(cleanCss({specialComments: 0}))
         .pipe(gulp.dest('dist/'));
-  
 });
 
-gulp.task('default', ['watch', 'css']);
+gulp.task('watch', function(){
+
+  // Serve files from the root of this project
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      },
+      notify: false
+  });
+
+  gulp.watch('src/main.css', ['tailwind']);
+  gulp.watch('build/main.css', ['css']);
+  gulp.watch(['./*.html', 'dist/*.css', './*.js']).on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['tailwind', 'css', 'watch']);
